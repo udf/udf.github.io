9c637f88108653679af8f5f6cd81e3e6d765d899
udf <tabhooked@gmail.com>
2026-04-07 19:31:23 +0200

    fix formatting

--- content/sections/memoriae-indefinitus/mksection.sh
+++ content/sections/memoriae-indefinitus/mksection.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -o noclobber
 
-date "+%Y-%m-%d" -d "$1" || exit 1
+if [ -z "$1" ]; then
+  date_str=$(date "+%Y-%m-%d")
+else
+  date_str=$(date "+%Y-%m-%d" -d "$1") || exit 1
+fi
 
-filename="$1.md"
+filename="$date_str.md"
 cat > "$filename" << EOF
 ---
 ---
